e9323181aca588518669bac805f0eed45eb28475
hlkean <hlkean@gmail.com>
2024-10-03 11:06:52 -0400

    initial commit of Astro set up

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import awsAmplify from 'astro-aws-amplify';
+
+// https://astro.build/config
+export default defineConfig({
+    output: "hybrid", 
+    adapter: awsAmplify(), 
+  });
